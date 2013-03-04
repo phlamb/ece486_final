9dde8ff0bd577b4a2a071167d5fbe50654843981
pjl <pjl@pdx.edu>
2013-03-04 12:42:40 -0800

    Added data structure for alpha predictor, updated get prediction

--- predictor.cc
+++ predictor.cc
@@ -2,22 +2,62 @@
 
 #include "predictor.h"
 
+bool PREDICTOR::get_prediction(const branch_record_c* br, const op_state_c* os, uint *predicted_target_address)
+{
+    bool prediction = get_branch_prediction(br, os);
+
+    get_target_prediction(br, os, predicted_target_address);
+
+    return prediction;
+}
+
+// Update the predictor after a prediction has been made.  This should accept
+// the branch record (br) and architectural state (os), as well as a third
+// argument (taken) indicating whether or not the branch was taken.
+void PREDICTOR::update_predictor(const branch_record_c* br, const op_state_c* os, bool taken, uint actual_target_address)
+{
+
+}
 
 
-    bool PREDICTOR::get_prediction(const branch_record_c* br, const op_state_c* os, uint *predicted_target_address)
-        {
-            bool prediction = true;
+//======================================
+// Branch Predictor Implementation
+// ====================================
+bool PREDICTOR::get_branch_prediction(const branch_record_c* br, const op_state_c* os)
+{
 
-            if (br->is_conditional)
-                prediction = false;
+    bool prediction = true;
+    if(!br->is_conditional)
+        return true;
 
-            return prediction;   // true for taken, false for not taken
-        }
+    if(chosePredictor == PREDICTOR_LOCAL)
+    {
+        int index = br->instruction_addr & 1024;
+        return (bool)alpha.localPrediction[localHistory[index]] & 0x1;
+    }
+    else //PREDICTOR_GLOBAL
+    {
+        return false; 
+    }
 
-    // Update the predictor after a prediction has been made.  This should accept
-    // the branch record (br) and architectural state (os), as well as a third
-    // argument (taken) indicating whether or not the branch was taken.
-    void PREDICTOR::update_predictor(const branch_record_c* br, const op_state_c* os, bool taken, uint actual_target_address)
-        {
 
-        }
+    return prediction;   // true for taken, false for not taken
+    
+}
+
+void PREDICTOR::update_branch_predictor(const branch_record_c* br, const op_state_c* os, bool taken)
+{
+
+}
+//======================================
+// Target Predictor Implementation
+// ====================================
+void PREDICTOR::get_target_prediction(const branch_record_c* br, const op_state_c* os, uint *predicted_target_address)
+{
+
+}
+
+void PREDICTOR::update_target_predictor(const branch_record_c* br, const op_state_c* os, bool taken, uint actual_target_address)
+{
+
+}
--- predictor.h
+++ predictor.h
@@ -13,14 +13,36 @@
 #include "op_state.h"   // defines op_state_c (architectural state) class 
 #include "tread.h"      // defines branch_record_c class
 
+#define PREDICTOR_LOCAL 0
+#define PREDICTOR_GLOBAL 1
+struct AlphaPredictor;
+
 class PREDICTOR
 {
 public:
     bool get_prediction(const branch_record_c* br, const op_state_c* os, uint *predicted_target_address);
-
     void update_predictor(const branch_record_c* br, const op_state_c* os, bool taken, uint actual_target_address);
+
+private:
+    bool get_branch_prediction(const branch_record_c* br, const op_state_c* os);
+    void get_target_prediction(const branch_record_c* br, const op_state_c* os, uint *predicted_target_address);
+
+    void update_branch_predictor(const branch_record_c* br, const op_state_c* os, bool taken);
+    void update_target_predictor(const branch_record_c* br, const op_state_c* os, bool taken, uint actual_target_address);
+
+    AlphaPredictorStorage alpha;
 
 };
 
+struct AlphaPredictorStorage
+{
+    uint16_t    localHistory[1024];     //Size: 10b x 1024
+    uint8_t     localPrediction[1024];  //Size: 3b x 1024
+    uint8_t     globalPrediction[4096]; //Size: 2b x 4096
+    uint8_t     choicePrediction[4096]; //Size: 2b x 4096
+    //Total Size: 3.712x10^3 Bytes 
+    // 2^(11.858) Bytes
+    // < 4k Bytes
+};
 #endif // PREDICTOR_H_SEEN
 
