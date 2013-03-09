7e1761597d6214d7cc05228ff49e79bb31336cfa
pjl <pjl@pdx.edu>
2013-03-09 10:57:18 -0800

    Added trace runner and updated makefile to build new binary

--- Makefile.bak
+++ Makefile.bak
@@ -5,13 +5,9 @@ CFLAGS = -g -Wall
 CXXFLAGS = -g -Wall
 
 objects = cbp_inst.o main.o op_state.o predictor.o tread.o
-tracerunobjects = cbp_inst.o op_state.o predictor.o tread.o
 
 predictor : $(objects)
 	$(CXX) -o $@ $(objects)
-
-tracerun : $(tracerunobjects) tracerun.c
-	$(CXX) -o $@ $(tracerunobjects) tracerun.c 
 
 cbp_inst.o : cbp_inst.h cbp_assert.h cbp_fatal.h cond_pred.h finite_stack.h indirect_pred.h stride_pred.h value_cache.h
 main.o : tread.h cbp_inst.h predictor.h op_state.h
---
+++ tracerun.c
@@ -0,0 +1,65 @@
+#include "predictor.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFSIZE 200
+
+int main(int argc, const char* argv[])
+{
+    FILE* fp;
+    char buf[BUFSIZE];
+    branch_record_c br;
+    uint target_addr;
+    bool taken;
+    PREDICTOR p;
+    uint predicted_addr;
+    bool predicted_taken;
+
+    if(argc < 2)
+    {
+        printf("No filename provided\n");
+        return -1;
+    }
+
+    printf("Opening file: %s\n", argv[1]);
+    fp = fopen(argv[1], "r");
+
+    if(!fp)
+    {
+        printf("Error reading file\n");
+        return -1;
+    }
+
+    printf("PC\tCond\tCall\tRet\tP_Tgt\tP_Taken\tTgt\tTaken\tTaken?\tTgt?\n");
+    while(fgets(buf, BUFSIZE - 1, fp) != NULL)
+    {
+
+        sscanf(buf, "%x %d %d %d %x %d", 
+                &br.instruction_addr, 
+                (int*)&br.is_conditional, 
+                (int*)&br.is_call, 
+                (int*)&br.is_return,
+                &target_addr,
+                (int*)&taken);
+
+        predicted_taken = p.get_prediction(&br, 0, &predicted_addr);
+
+        printf("%X\t%d\t%d\t%d\t%X\t%d\t%X\t%d\t%d\t%d\n",
+                br.instruction_addr,
+                br.is_conditional,
+                (int)br.is_call,
+                (int)br.is_return,
+                predicted_addr,
+                (int)predicted_taken,
+                target_addr,
+                taken,
+                (int)(taken == predicted_taken),
+                (int)(target_addr == predicted_addr));
+
+
+        p.update_predictor(&br, 0, taken, target_addr);
+
+    }
+
+}
